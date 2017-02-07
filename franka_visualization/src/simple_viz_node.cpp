819d6aa9dfa5afa9ce337bc0bfc2a9af3b3b8594
Christoph Jaehne <christoph.jaehne@franka.de>
2017-02-07 13:53:41 +0100

    included libfranka to read from robot

--- franka_visualization/src/simple_viz_node.cpp
+++ franka_visualization/src/simple_viz_node.cpp
@@ -1,52 +1,66 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <franka/robot.h>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "simple_viz_node");
-  ros::NodeHandle nh("~");
-  ros::NodeHandle nhp;
-  ros::Rate rate(1000);
-  ros::Publisher joint_pub =
-      nhp.advertise<sensor_msgs::JointState>("joint_state", 1);
-  std::vector<std::string> joint_names;
-  sensor_msgs::JointState states;
+    ros::init(argc, argv, "simple_viz_node");
+    ros::NodeHandle nh("~");
+    ros::NodeHandle nhp;
+    ros::Rate rate(1000);
+    ros::Publisher joint_pub =
+            nhp.advertise<sensor_msgs::JointState>("joint_states", 1);
+    std::vector<std::string> joint_names;
 
-  // parse yaml with joint names into joint_names..
-  XmlRpc::XmlRpcValue params;
-  nh.getParam("joint_names", params);
-  joint_names.resize(params.size());
-  for (int i = 0; i < params.size(); ++i) {
-    joint_names[i] = static_cast<std::string>(params[i]);
-    ROS_INFO("joint %d: %s", i, joint_names[i].c_str());
-  }
-
-  states.effort.resize(joint_names.size());
-  states.name.resize(joint_names.size());
-  states.position.resize(joint_names.size());
-  states.velocity.resize(joint_names.size());
-
-  std::vector<double> dummy;
-  dummy.resize(joint_names.size());
-  for (int i = 0; i < dummy.size(); ++i) {
-    dummy[i] = 0.0f;
-  }
-
-  while (ros::ok()) {
-    // TODO: read sensors from franka using libfranka
-
-    // update joint_states to msgs
-    states.header.stamp = ros::Time::now();
-    for (int i = 0; i < joint_names.size(); ++i) {
-      states.name.at(i) = joint_names.at(i);
-      states.position.at(i) = dummy.at(i);
-      states.velocity.at(i) = dummy.at(i);
-      states.effort.at(i) = dummy.at(i);
+    // parse yaml with joint names and robot-IP from yaml
+    XmlRpc::XmlRpcValue params;
+    nh.getParam("joint_names", params);
+    joint_names.resize(params.size());
+    for (int i = 0; i < params.size(); ++i) {
+        joint_names[i] = static_cast<std::string>(params[i]);
+        ROS_INFO("joint %d: %s", i, joint_names[i].c_str());
     }
+    std::string robot_ip;
+    nh.getParam("robot_ip", robot_ip);
+    ROS_INFO("parsed IP to connect to: %s", robot_ip.c_str());
 
-    joint_pub.publish(states);
-    ros::spinOnce();
-    rate.sleep();
-  }
+    sensor_msgs::JointState states;
+    states.effort.resize(joint_names.size());
+    states.name.resize(joint_names.size());
+    states.position.resize(joint_names.size());
+    states.velocity.resize(joint_names.size());
 
-  return 0;
-}
+
+    try
+    {
+        long int secnr = 1;
+        franka::Robot robot(robot_ip);
+        while (ros::ok() && robot.waitForRobotState())
+        {
+            // read sensors from franka using libfranka
+            const franka::RobotState& robotState = robot.getRobotState();
+            // std::cout << robotState << std::endl;
+            
+            // update joint_states to msgs
+            states.header.stamp = ros::Time::now();
+            states.header.seq = secnr;
+            for (int i = 0; i < joint_names.size(); ++i)
+            {
+                states.name.at(i) = joint_names.at(i);
+                states.position.at(i) = robotState.q[i];
+                states.velocity.at(i) = robotState.dq[i];
+                states.effort.at(i) = robotState.tau_J[i];
+            }
+            joint_pub.publish(states);
+            ros::spinOnce();
+            rate.sleep();
+            secnr++;
+        }
+    }
+    catch(franka::NetworkException const& e)
+    {
+        std::cout << e.what() << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl ;
+        return -1;
+    }
+    
+    return 0;
+}    
