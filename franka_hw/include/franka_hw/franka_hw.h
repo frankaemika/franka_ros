300f933ef50e2549df58af9de77b2b7c8580f4ee
Christoph Jaehne <christoph.jaehne@franka.de>
2017-02-14 10:09:00 +0100

    adapted state interfaces

--- franka_hw/include/franka_hw/franka_hw.h
+++ franka_hw/include/franka_hw/franka_hw.h
@@ -29,13 +29,10 @@ class FrankaHW : public hardware_interface::RobotHW {
   void updateStates(const franka::RobotState& robot_state);
 
  private:
-  hardware_interface::JointStateInterface joint_state_interface_;  // interfaces
+  hardware_interface::JointStateInterface joint_state_interface_;
   hardware_interface::FrankaJointStateInterface franka_joint_state_interface_;
-  hardware_interface::FrankaCartesianStateInterface
-      franka_cartesian_state_interface_;
-
-  franka::Robot robot_;  // libfranka robot
-
+  hardware_interface::FrankaCartesianStateInterface franka_cartesian_state_interface_;
+  franka::Robot robot_;
   realtime_tools::RealtimePublisher<franka_hw::FrankaState> publisher_franka_states_;
   realtime_tools::RealtimePublisher<sensor_msgs::JointState> publisher_joint_states_;
   uint64_t sequence_number_joint_states_ = 0;
@@ -43,25 +40,8 @@ class FrankaHW : public hardware_interface::RobotHW {
   uint64_t missed_pulishes_franka_states_ = 0;
   uint64_t missed_pulishes_joint_states_ = 0;
 
-  std::vector<std::string> joint_name_;  // joint_names
+  std::vector<std::string> joint_name_;
   franka::RobotState robot_state_;
-  /*
-  std::array<double, 7> q_;  // robot state variables
-  std::array<double, 7> dq_;
-  std::array<double, 7> q_d_;
-  std::array<double, 7> q_start_;
-  std::array<double, 7> tau_J_;
-  std::array<double, 7> dtau_J_;
-  std::array<double, 7> tau_ext_hat_filtered_;
-  std::array<double, 6> cartesian_collision_;
-  std::array<double, 6> cartesian_contact_;
-  std::array<double, 7> joint_collision_;
-  std::array<double, 7> joint_contact_;
-  std::array<double, 6> EE_F_ext_hat_EE_;
-  std::array<double, 6> O_F_ext_hat_EE_;
-  std::array<double, 2> elbow_start_;
-  std::array<std::array<double, 4>, 4> O_T_EE_start_;
-  */
 };
 
 }  // namespace franka_hw
