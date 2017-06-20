65aec15a13e9f51922d5ab5f229356165f1efc6e
Florian Walch <florian.walch@franka.de>
2017-06-20 17:55:49 +0200

    Add move_to_start script.

---
+++ franka_moveit_config/scripts/move_to_start.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import rospy
+from moveit_commander import MoveGroupCommander
+from actionlib_msgs.msg import GoalStatusArray
+
+if __name__ == '__main__':
+    rospy.init_node('move_to_start')
+    rospy.wait_for_message('move_group/status', GoalStatusArray)
+    commander = MoveGroupCommander('franka')
+    commander.set_named_target('ready')
+    commander.go()
