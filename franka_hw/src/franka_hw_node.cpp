77f890f91b7543458dbe5dbacfaa6c1313de2ed7
Florian Walch <florian.walch@franka.de>
2017-07-20 16:19:52 +0200

    Use free functions to advertise services.

--- franka_hw/src/franka_hw_node.cpp
+++ franka_hw/src/franka_hw_node.cpp
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <controller_manager/controller_manager.h>
@@ -9,7 +10,21 @@
 
 #include <franka/robot.h>
 #include <franka_hw/franka_hw.h>
-#include <franka_hw/service_server.h>
+#include <franka_hw/services.h>
+
+class ServiceContainer {
+ public:
+  template <typename T, typename... TArgs>
+  ServiceContainer& advertiseService(TArgs&&... args) {
+    ros::ServiceServer server =
+        franka_hw::services::advertiseService<T>(std::forward<TArgs>(args)...);
+    services_.push_back(server);
+    return *this;
+  }
+
+ private:
+  std::vector<ros::ServiceServer> services_;
+};
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "franka_hw");
@@ -37,13 +52,52 @@ int main(int argc, char** argv) {
                              {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}},
                              {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}});
 
+  std::atomic_bool has_error(false);
+
+  using std::placeholders::_1;
+  using std::placeholders::_2;
+  ServiceContainer services;
+  services
+      .advertiseService<franka_hw::SetJointImpedance>(
+          node_handle, "set_joint_impedance",
+          std::bind(franka_hw::services::setJointImpedance, std::ref(robot), _1,
+                    _2))
+      .advertiseService<franka_hw::SetCartesianImpedance>(
+          node_handle, "set_cartesian_impedance",
+          std::bind(franka_hw::services::setCartesianImpedance, std::ref(robot),
+                    _1, _2))
+      .advertiseService<franka_hw::SetEEFrame>(
+          node_handle, "set_EE_frame",
+          std::bind(franka_hw::services::setEEFrame, std::ref(robot), _1, _2))
+      .advertiseService<franka_hw::SetKFrame>(
+          node_handle, "set_K_frame",
+          std::bind(franka_hw::services::setKFrame, std::ref(robot), _1, _2))
+      .advertiseService<franka_hw::SetForceTorqueCollisionBehavior>(
+          node_handle, "set_force_torque_collision_behavior",
+          std::bind(franka_hw::services::setForceTorqueCollisionBehavior,
+                    std::ref(robot), _1, _2))
+      .advertiseService<franka_hw::SetFullCollisionBehavior>(
+          node_handle, "set_full_collision_behavior",
+          std::bind(franka_hw::services::setFullCollisionBehavior,
+                    std::ref(robot), _1, _2))
+      .advertiseService<franka_hw::SetLoad>(
+          node_handle, "set_load",
+          std::bind(franka_hw::services::setLoad, std::ref(robot), _1, _2))
+      .advertiseService<franka_hw::SetTimeScalingFactor>(
+          node_handle, "set_time_scaling_factor",
+          std::bind(franka_hw::services::setTimeScalingFactor, std::ref(robot),
+                    _1, _2))
+      .advertiseService<franka_hw::ErrorRecovery>(
+          node_handle, "error_recovery",
+          [&](const franka_hw::ErrorRecovery::Request& req,
+              franka_hw::ErrorRecovery::Response& res) {
+            franka_hw::services::errorRecovery(robot, req, res);
+            has_error = false;
+          });
+
   franka_hw::FrankaHW franka_control(joint_names, &robot, arm_id, node_handle);
   controller_manager::ControllerManager control_manager(&franka_control,
                                                         node_handle);
-
-  std::atomic_bool has_error(false);
-  franka_hw::ServiceServer service_server(
-      robot, node_handle, {{"error_recovery", [&]() { has_error = false; }}});
 
   // Start background thread for message handling
   ros::AsyncSpinner spinner(1);
