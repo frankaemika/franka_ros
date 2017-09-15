61d9f0555645489159a424433f5faec4c3f6d815
Florian Walch <florian.walch@franka.de>
2017-09-15 14:41:49 +0200

    Rename franka_moveit_config to panda_moveit_config.

--- panda_moveit_config/scripts/move_to_start.py
+++ panda_moveit_config/scripts/move_to_start.py
@@ -7,6 +7,6 @@ from actionlib_msgs.msg import GoalStatusArray
 if __name__ == '__main__':
     rospy.init_node('move_to_start')
     rospy.wait_for_message('move_group/status', GoalStatusArray)
-    commander = MoveGroupCommander('franka')
+    commander = MoveGroupCommander('panda_arm')
     commander.set_named_target('ready')
     commander.go()
