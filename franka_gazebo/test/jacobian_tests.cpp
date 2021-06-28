4adb450f218c576631bf5e52cc51941d57769de0
Thore Goll <thore.goll@franka.de>
2021-06-28 10:41:56 +0200

    ADD: Implementation for Body Jacobian in `ModelKDL`
    KDL offers a method to change the base of the jacobians, with which the velocity
    mappings can be expressed in an arbitrary frame, not only the O frame.
    
    Meaning to calculate the body jacobian out of the zero jacobian (relative to O
    frame) we simple call this `KDL::Jacobian::changeBase()` function and give it
    the rotation to change the basis.
    
    This rotation comes from the FK of that frame, but has to be inverted for some
    reason (not completely sure). The result of the calculations are checked vs the
    model library with unit tests.
    
    NOTE: There is also a `KDL::Jacobian::changeRefFrame()` function, which takes
    also the position of the frame into account, which on first glance sounds more
    fitting. However this does not match the behavior of our model library.

--- franka_gazebo/test/jacobian_tests.cpp
+++ franka_gazebo/test/jacobian_tests.cpp
@@ -36,7 +36,7 @@ class JacobianFixture : public ::testing::Test {
   virtual void TearDown() {}
 };
 
-TEST_F(JacobianFixture, jac_joint1_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint1_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint1, q, identity, identity);
@@ -56,7 +56,7 @@ TEST_F(JacobianFixture, jac_joint1_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint2_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint2_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint2, q, identity, identity);
@@ -76,7 +76,7 @@ TEST_F(JacobianFixture, jac_joint2_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint3_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint3_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint3, q, identity, identity);
@@ -96,7 +96,7 @@ TEST_F(JacobianFixture, jac_joint3_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint4_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint4_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint4, q, identity, identity);
@@ -116,7 +116,7 @@ TEST_F(JacobianFixture, jac_joint4_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint5_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint5_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint5, q, identity, identity);
@@ -136,7 +136,7 @@ TEST_F(JacobianFixture, jac_joint5_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint6_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint6_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint6, q, identity, identity);
@@ -156,7 +156,7 @@ TEST_F(JacobianFixture, jac_joint6_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_joint7_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_joint7_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kJoint7, q, identity, identity);
@@ -176,7 +176,7 @@ TEST_F(JacobianFixture, jac_joint7_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_flange_zero_pose) {
+TEST_F(JacobianFixture, zero_jacobian_flange_zero_pose) {
   std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kFlange, q, identity, identity);
@@ -196,7 +196,7 @@ TEST_F(JacobianFixture, jac_flange_zero_pose) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_flange_elbow_bend_90deg) {
+TEST_F(JacobianFixture, zero_jacobian_flange_elbow_bend_90deg) {
   std::array<double, 7> q = {0, 0, 0, -M_PI / 2, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kFlange, q, identity, identity);
@@ -217,7 +217,7 @@ TEST_F(JacobianFixture, jac_flange_elbow_bend_90deg) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_flange_elbow_bend_120deg) {
+TEST_F(JacobianFixture, zero_jacobian_flange_elbow_bend_120deg) {
   std::array<double, 7> q = {0, 0, 0, -120.0 * M_PI / 180.0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kFlange, q, identity, identity);
@@ -237,7 +237,7 @@ TEST_F(JacobianFixture, jac_flange_elbow_bend_120deg) {
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_flange_shoulder_panned_35deg_and_tilted_minus17deg) {
+TEST_F(JacobianFixture, zero_jacobian_flange_shoulder_panned_35deg_and_tilted_minus17deg) {
   std::array<double, 7> q = {35 * M_PI / 180.0, -17 * M_PI / 180.0, 0, 0, 0, 0, 0};
   std::array<double, 42> jacobian =
       model->zeroJacobian(franka::Frame::kFlange, q, identity, identity);
@@ -257,7 +257,7 @@ TEST_F(JacobianFixture, jac_flange_shoulder_panned_35deg_and_tilted_minus17deg) 
   compareMatricesElementWise(expected, actual, 0.01);
 }
 
-TEST_F(JacobianFixture, jac_flange_random_pose) {
+TEST_F(JacobianFixture, zero_jacobian_flange_random_pose) {
   std::array<double, 7> q = {0.5157262388785411,  1.2140897359597562,  1.5346381355065786,
                              -3.0398301021734246, -1.2930720893855998, 1.332867311125138,
                              -1.5554459725458225};
@@ -276,5 +276,259 @@ TEST_F(JacobianFixture, jac_flange_random_pose) {
             0,    0.869935,    0.462123,    0.140648,   -0.370783,    0.921392,  -0.0257594,
             1,           0,     0.34919,   -0.936439,   -0.350825,   -0.255036,   -0.792973;
   // clang-format on
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint1_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint1, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      1,    0,    0,    0,    0,    0,    0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint2_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint2, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+      0,    0,    0,    0,    0,    0,    0,
+     -1,    0,    0,    0,    0,    0,    0,
+      0,    1,    0,    0,    0,    0,    0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint3_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint3, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+    expected <<
+        0,  0.316,     0,     0,     0,     0,     0,
+        0,      0,     0,     0,     0,     0,     0,
+        0,      0,     0,     0,     0,     0,     0,
+        0,      0,     0,     0,     0,     0,     0,
+        0,      1,     0,     0,     0,     0,     0,
+        1,      0,     1,     0,     0,     0,     0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint4_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint4, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+    expected <<
+          0,   0.316,       0,       0,       0,       0,       0,
+          0, -0.0825,       0,       0,       0,       0,       0,
+    -0.0825,       0, -0.0825,       0,       0,       0,       0,
+          0,       0,       0,       0,       0,       0,       0,
+          1,       0,       1,       0,       0,       0,       0,
+          0,      -1,       0,       1,       0,       0,       0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint5_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint5, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+    expected <<
+          0,     0.7,       0,  -0.384,       0,       0,       0,
+          0,       0,       0,       0,       0,       0,       0,
+          0,       0,       0, -0.0825,       0,       0,       0,
+          0,       0,       0,       0,       0,       0,       0,
+          0,       1,       0,      -1,       0,       0,       0,
+          1,       0,       1,       0,       1,       0,       0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint6_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint6, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+        0,     0.7,       0,  -0.384,       0,       0,       0,
+        0,       0,       0, -0.0825,       0,       0,       0,
+        0,       0,       0,       0,       0,       0,       0,
+        0,       0,       0,       0,       0,       0,       0,
+        1,       0,       1,       0,       1,       0,       0,
+        0,      -1,       0,       1,       0,       1,       0;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_joint7_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kJoint7, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+          0,     0.7,       0,  -0.384,       0,       0,       0,
+     -0.088,       0,  -0.088,       0,  -0.088,       0,       0,
+          0,   0.088,       0, -0.0055,       0,  -0.088,       0,
+          0,       0,       0,       0,       0,       0,       0,
+          0,      -1,       0,       1,       0,       1,       0,
+         -1,       0,      -1,       0,      -1,       0,       1;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_flange_zero_pose) {
+  std::array<double, 7> q = {0, 0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kFlange, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+        0,   0.593,       0,  -0.277,       0,   0.107,       0,
+   -0.088,       0,  -0.088,       0,  -0.088,       0,       0,
+        0,   0.088,       0, -0.0055,       0,  -0.088,       0,
+        0,       0,       0,       0,       0,       0,       0,
+        0,      -1,       0,       1,       0,       1,       0,
+       -1,       0,      -1,       0,      -1,       0,       1;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_flange_elbow_bend_90deg) {
+  std::array<double, 7> q = {0, 0, 0, -M_PI / 2, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kFlange, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+           0,    0.3595,         0,    -0.277,        0,    0.107,     0,
+     -0.3595,         0,   -0.3595,         0,   -0.088,        0,     0,
+           0,   -0.3105,         0,   -0.0055,        0,   -0.088,     0,
+          -1,         0,        -1,         0,        0,        0,     0,
+           0,        -1,         0,         1,        0,        1,     0,
+           0,         0,         0,         0,       -1,        0,     1;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_flange_elbow_bend_120deg) {
+  std::array<double, 7> q = {0, 0, 0, -120.0 * M_PI / 180.0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kFlange, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+         0,     0.1904,          0,     -0.277,         0,     0.107,       0,
+   -0.3196,          0,    -0.3196,          0,    -0.088,         0,       0,
+         0,    -0.3094,          0,    -0.0055,         0,    -0.088,       0,
+   -0.8660,          0,    -0.8660,          0,         0,         0,       0,
+         0,         -1,          0,          1,         0,         1,       0,
+       0.5,          0,        0.5,          0,        -1,        -0,       1;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_flange_shoulder_panned_35deg_and_tilted_minus17deg) {
+  std::array<double, 7> q = {35 * M_PI / 180.0, -17 * M_PI / 180.0, 0, 0, 0, 0, 0};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kFlange, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+          0,     0.593,         0,    -0.277,        0,    0.107,      0,
+     0.0892,        -0,    -0.088,         0,   -0.088,        0,      0,
+          0,     0.088,         0,   -0.0055,        0,   -0.088,      0,
+     0.2923,         0,         0,         0,        0,        0,      0,
+         -0,        -1,         0,         1,        0,        1,      0,
+    -0.9563,         0,        -1,         0,       -1,        0,      1;
+  // clang-format on
+
+  compareMatricesElementWise(expected, actual, 0.01);
+}
+
+TEST_F(JacobianFixture, body_jacobian_flange_random_pose) {
+  std::array<double, 7> q = {0.5157262388785411,  1.2140897359597562,  1.5346381355065786,
+                             -3.0398301021734246, -1.2930720893855998, 1.332867311125138,
+                             -1.5554459725458225};
+  std::array<double, 42> jacobian =
+      model->bodyJacobian(franka::Frame::kFlange, q, identity, identity);
+
+  Eigen::Matrix<double, 6, 7> expected, actual;
+  actual = Eigen::Matrix<double, 6, 7>(jacobian.data());
+
+  // clang-format off
+  expected <<
+      0.101939,    0.051154,    -0.067125,   -0.42838,   0.12471,   0.00164,     0,
+      0.139199,    0.094503,    -0.034664,   -0.06908,  -0.00191,   0.10698,     0,
+     -0.066113,   -0.138015,    -0.198863,   -0.10699,   0      ,  -0.088  ,     0,
+      0.246513,   -0.945656,    -0.112624,   -0.27065,   0.01491,  -0.99988,     0,
+     -0.557158,    0.040744,    -0.971751,    0.23084,   0.97171,   0.01534,     0,
+     -0.792973,   -0.322605,     0.207404,    0.93459,  -0.23569,   0      ,     1;
+  // clang-format on
+
   compareMatricesElementWise(expected, actual, 0.01);
 }
