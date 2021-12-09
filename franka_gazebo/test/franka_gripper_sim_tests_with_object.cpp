c209f06a2a46b981ed0e8b3b97e85e61dd53f870
Marco Boneberger <marco.boneberger@franka.de>
2021-12-09 15:41:28 +0100

    add tests for grasp epsilon

--- franka_gazebo/test/franka_gripper_sim_tests_with_object.cpp
+++ franka_gazebo/test/franka_gripper_sim_tests_with_object.cpp
@@ -14,17 +14,21 @@ static const double kStoneWidth = 0.032;
 
 class GripperGraspFixtureTest
     : public GripperSimTestSetup,
-      public testing::WithParamInterface<std::tuple<std::tuple<double, double, double>, double>> {
+      public testing::WithParamInterface<
+          std::tuple<std::tuple<double, double, double, double, double>, double>> {
  protected:
   void SetUp() override {
     GripperSimTestSetup::SetUp();
-    std::tie(desired_width, desired_velocity, desired_force) = std::get<0>(GetParam());
+    std::tie(desired_width, desired_velocity, desired_force, desired_epsilon_inner,
+             desired_epsilon_outer) = std::get<0>(GetParam());
     desired_sleep = std::get<1>(GetParam());
   }
 
   double desired_width{0};
   double desired_velocity{0};
   double desired_force{0};
+  double desired_epsilon_inner{0};
+  double desired_epsilon_outer{0};
   double desired_sleep{0};
 };
 
@@ -66,8 +70,8 @@ TEST_P(GripperGraspFixtureTest, CanGrasp) {  // NOLINT(cert-err58-cpp)
   grasp_goal.width = desired_width;
   grasp_goal.speed = desired_velocity;
   grasp_goal.force = desired_force;
-  grasp_goal.epsilon.inner = 0.005;
-  grasp_goal.epsilon.outer = 0.005;
+  grasp_goal.epsilon.inner = desired_epsilon_inner;
+  grasp_goal.epsilon.outer = desired_epsilon_outer;
   auto start_time = ros::Time::now();
   this->grasp_client->sendGoal(grasp_goal);
   finished_before_timeout = this->grasp_client->waitForResult(ros::Duration(10.0));
@@ -99,8 +103,8 @@ TEST_P(GripperFailGraspFixtureTest, CanFailGrasp) {  // NOLINT(cert-err58-cpp)
   grasp_goal.width = desired_width;
   grasp_goal.speed = desired_velocity;
   grasp_goal.force = desired_force;
-  grasp_goal.epsilon.inner = 0.005;
-  grasp_goal.epsilon.outer = 0.005;
+  grasp_goal.epsilon.inner = desired_epsilon_inner;
+  grasp_goal.epsilon.outer = desired_epsilon_outer;
   auto start_time = ros::Time::now();
   this->grasp_client->sendGoal(grasp_goal);
   finished_before_timeout = this->grasp_client->waitForResult(ros::Duration(10.0));
@@ -115,19 +119,22 @@ TEST_P(GripperFailGraspFixtureTest, CanFailGrasp) {  // NOLINT(cert-err58-cpp)
   EXPECT_FALSE(grasp_client->getResult()->success);
 }
 
-INSTANTIATE_TEST_CASE_P(GripperFailGraspFixtureTest,  // NOLINT(cert-err58-cpp)
-                        GripperFailGraspFixtureTest,
-                        ::testing::Combine(::testing::Values(std::make_tuple(0.04, 0.1, 0.),
-                                                             std::make_tuple(0.02, 0.1, 2.)),
-                                           ::testing::Values(0, 0.1)));
+INSTANTIATE_TEST_CASE_P(
+    GripperFailGraspFixtureTest,  // NOLINT(cert-err58-cpp)
+    GripperFailGraspFixtureTest,
+    ::testing::Combine(::testing::Values(std::make_tuple(0.04, 0.1, 0., 0.005, 0.005),
+                                         std::make_tuple(0.02, 0.1, 2., 0.01, 0.01),
+                                         std::make_tuple(0.03, 0.1, 2, 0.001, 0.001)),
+                       ::testing::Values(0, 0.1)));
 
-INSTANTIATE_TEST_CASE_P(GripperGraspFixtureTest,  // NOLINT(cert-err58-cpp)
-                        GripperGraspFixtureTest,
-                        ::testing::Combine(::testing::Values(std::make_tuple(0.032, 0.1, 0.),
-                                                             std::make_tuple(0.03, 0.1, 5.),
-                                                             std::make_tuple(0.03, 0.01, 0.),
-                                                             std::make_tuple(0.034, 0.01, 5.)),
-                                           ::testing::Values(0, 0.1)));
+INSTANTIATE_TEST_CASE_P(
+    GripperGraspFixtureTest,  // NOLINT(cert-err58-cpp)
+    GripperGraspFixtureTest,
+    ::testing::Combine(::testing::Values(std::make_tuple(0.032, 0.1, 0., 0.01, 0.01),
+                                         std::make_tuple(0.03, 0.1, 5., 0.005, 0.005),
+                                         std::make_tuple(0.0325, 0.01, 0., 0.001, 0.001),
+                                         std::make_tuple(0.034, 0.01, 5., 0.005, 0.005)),
+                       ::testing::Values(0, 0.1)));
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
